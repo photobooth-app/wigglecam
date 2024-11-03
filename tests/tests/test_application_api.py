6f9c88218e3014708395df8b7b1e278f8fd65451
mgineer85 <me@mgineer85.de>
2024-11-03 14:55:55 +0100

    remove not needed test.

--- tests/tests/test_application_api.py
+++ tests/tests/test_application_api.py
@@ -3,10 +3,6 @@ Testing virtual camera Backend
 """
 
 import logging
-import os
-from unittest.mock import patch
-
-import pytest
 
 logger = logging.getLogger(name=None)
 
@@ -21,12 +17,3 @@ def test_main_instance():
     import wigglecam.__main__
 
     wigglecam.__main__.main(False)
-
-
-def test_main_instance_create_dirs_permission_error():
-    from wigglecam.__main__ import create_basic_folders
-
-    with patch.object(os, "makedirs", side_effect=RuntimeError("effect: failed creating folder")):
-        # emulate write access issue and ensure an exception is received to make the app fail starting.
-        with pytest.raises(RuntimeError):
-            create_basic_folders()
